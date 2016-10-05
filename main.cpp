b958be01b7af2e6633cefd7ba0c9ba12c6c7a572
mischa1998 <mischa1998@inbox.ru>
2016-10-05 23:43:04 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdlib.h>
+#include "Triangle.h"
+#include "Hexagon.h"
+#include "Octagon.h"
+using namespace std;
+
+
+int main()
+{
+	char c;
+	while ((c = getchar()) != 'e') {
+		if (c == 'T') {	
+	    	Triangle *ptr = new Triangle;
+	    	ptr->Print();
+	    	cout<<"Square is "<<ptr->Square()<<endl;
+	    	delete ptr;
+		}
+		if (c == 'H') {
+			Hexagon *ptr = new Hexagon;
+			ptr->Print();
+			cout<<"Square is "<<ptr->Square()<<endl;
+			delete ptr;
+		}
+		if (c == 'O') {
+			Octagon *ptr = new Octagon;
+			ptr->Print();
+			cout<<"Square is "<<ptr->Square()<<endl;
+			delete ptr;
+		}
+	}
+    return 0;
+}
